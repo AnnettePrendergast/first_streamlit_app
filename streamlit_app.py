ea428ca81cbe305fa6df84128665908ecbc137cc
AnnettePrendergast <122783253+AnnettePrendergast@users.noreply.github.com>
2023-01-16 10:49:56 +0000

    Update streamlit_app.py
    Menu items now added

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blackberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard=boiled Free Range Egg')
